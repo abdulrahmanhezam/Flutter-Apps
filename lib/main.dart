f4381228b6ab665ff0f61dae3851692e1a4e6a79
Abdulrahman Hezam <m.abdulrahman.hezam@gmail.com>
2020-07-05 14:03:04 +0300

    add th "%"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( debugShowCheckedModeBanner: false,  home: HomePage(), );
+    return MaterialApp(
+
+      theme: ThemeData.dark(),
+
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
   }
 }
 
@@ -19,44 +26,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+
+ divByZero(String message) async => Fluttertoast.showToast( //  تقوم هذه الدالة باظهار تنبيه عند القسمة على صفر
+        msg: message,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.redAccent,
+        textColor: Colors.white,
+        fontSize: 16.0);
+
   String output = ""; // متغير لتخزين العمليات في الاله الحاسبة
 
   String _output = ""; // يقوم باخراج العمليات الحسابية
-  double num1 = 0.0; //  متغير لتخزين الرقم الاول
-  double num2 = 0.0; // متغير لتخزين الرقم الثاني
+  double num1 = 0; //  متغير لتخزين الرقم الاول
+  double num2 = 0; // متغير لتخزين الرقم الثاني
   String operand = ""; // متغير يخزن العمليات الحسابية كاالضرب والطرح
 
   buttonPressed(String buttonText) {
-    // الدالة ما ترجع اي قيمة 
-	// تقوم الدالة باستقبال نص عبارة عن نص الزر 
-	// تقوم الدالة ب جميع العمليات الحسابية داخل التطبيق
-	
-	// buttonText  متغير نصي الهدف ان يحتوي على قيمة النص للازرار
-	
+    // الدالة ما ترجع اي قيمة
+    // تقوم الدالة باستقبال نص عبارة عن نص الزر
+    // تقوم الدالة ب جميع العمليات الحسابية داخل التطبيق
+
+    // buttonText  متغير نصي الهدف ان يحتوي على قيمة النص للازرار
+
     if (buttonText == "C") {
-      // السي هنا اختصار لكلمة clear وتعني مسح جميع البيانات السابقة 
-      num1 = 0.0;
-      num2 = 0.0;
+      // السي هنا اختصار لكلمة clear وتعني مسح جميع البيانات السابقة
+      num1 = 0;
+      num2 = 0;
       operand = "";
       _output = "";
-    } else if (buttonText == "+" || 
+    } else if (buttonText == "+" ||
         buttonText == "-" ||
         buttonText == "/" ||
-        buttonText == "X") { // هنا شرط ازرار العمليات الحسابية 
+        buttonText == "X" ||
+        buttonText == "%") {
+      // هنا شرط ازرار العمليات الحسابية
       num1 = double.parse(output);
       operand = buttonText;
       _output = "";
-    } else if (buttonText == '.') {
-      // اذا الرقم يحتوي على  "." من الاساس  ما يحتاج فاصلة اخرى
-      if (_output.contains('.')) {
-        print("Already Contains");
-        return;
-      } else {
-        _output = buttonText;
-      }
-    } else if (buttonText == '=') {
+    } else if (operand == "%") {
+      _output = (num1 / 100).toString();
+    }
+    else if (buttonText == '=') {
       // شرط عملية المساواة
       num2 = double.parse(output);
+
       if (operand == "+") {
         _output = (num1 + num2).toString(); // شرط عملية الجمع
       }
@@ -70,16 +86,16 @@ class _HomePageState extends State<HomePage> {
         _output = (num1 / num2).toString(); // شرط عملية القسمه
         if (num2 == 0) {
           // اذا كانت القسمه على على صفر الناتج غير معرف"notDefined"
-          _output = "Division by zero is not defined!";
+          _output = divByZero("يتعذر القسمة على صفر.") as String;
         }
       }
-      num1 = 0.0;
-      num2 = 0.0;
+      num1 = 0;
+      num2 = 0;
       operand = "";
     } else {
-      _output = _output + buttonText; 
+      _output = _output + buttonText;
     }
-	
+
     print(_output);
     setState(() {
       output = _output; // تقوم بعرض العمليات  اعلى
@@ -88,21 +104,25 @@ class _HomePageState extends State<HomePage> {
 
   // ignore: non_constant_identifier_names
   Widget theNumbers({String ButtonText, Color color}) {
-  
     //  هنا نقوم بعمل الزر
     return Expanded(
-      child: OutlineButton(
-        // نوع الزر المستخدم
-        color: color,
-        padding: EdgeInsets.all(24),
-        child: Text(
-          ButtonText,
-          style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+      child: Padding(
+        padding: const EdgeInsets.all(3),
+        child: FlatButton(
+          // نوع الزر المستخدم
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          color: color,
+          padding: EdgeInsets.all(22),
+          child: Text(
+            ButtonText,
+            style: TextStyle(
+                fontSize: 34, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          onPressed: () {
+            buttonPressed(ButtonText);
+            // هنا نقوم باستدعاء  دالة العمليات داخل الزر
+          },
         ),
-        onPressed: () {
-          buttonPressed(
-              ButtonText); // هنا نقوم باستدعاء  دالة العمليات داخل الزر
-        },
       ),
     );
   }
@@ -117,62 +137,59 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            //  هنا حيث شاشة عرض المخرجات
-            padding: EdgeInsets.all(28),
-            alignment: Alignment.topRight,
-            child: Text(
-              output,
-              style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
-            ),
-          ),
           Expanded(
-            child: new Divider(
-            color: Colors.black,
-            height: 20,
-            thickness: 5,
-            indent: 20,
-            endIndent: 0,),
+            child: Container(
+              //  هنا حيث شاشة عرض المخرجات
+              padding: EdgeInsets.all(8),
+              alignment: Alignment.topRight,
+              child: Text(
+                output.trim(),
+                style: TextStyle(
+                    fontSize: 38,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ),
           ),
 
           // هنا ترتيب الازرار على شكل صفوف
           Row(
             children: <Widget>[
-              TheNumbers(ButtonText: "C", color: Colors.amber),
+              theNumbers(ButtonText: "C", color: Colors.amber),
 //              TheNumbers(ButtonText: "()", color: Colors.amber),
-//              TheNumbers(ButtonText: "%", color: Colors.amber),
-              TheNumbers(ButtonText: "/", color: Colors.amber),
+              theNumbers(ButtonText: "%", color: Colors.amber),
+              theNumbers(ButtonText: "/", color: Colors.amber),
             ],
           ),
           Row(
             children: <Widget>[
-              TheNumbers(ButtonText: "7", color: Colors.white),
-              TheNumbers(ButtonText: "8", color: Colors.white),
-              TheNumbers(ButtonText: "9", color: Colors.white),
-              TheNumbers(ButtonText: "X", color: Colors.amber),
+              theNumbers(ButtonText: "7", color: Colors.white38),
+              theNumbers(ButtonText: "8", color: Colors.white38),
+              theNumbers(ButtonText: "9", color: Colors.white38),
+              theNumbers(ButtonText: "X", color: Colors.amber),
             ],
           ),
           Row(
             children: <Widget>[
-              TheNumbers(ButtonText: "4", color: Colors.white),
-              TheNumbers(ButtonText: "5", color: Colors.white),
-              TheNumbers(ButtonText: "6", color: Colors.white),
-              TheNumbers(ButtonText: "-", color: Colors.amber),
+              theNumbers(ButtonText: "4", color: Colors.white38),
+              theNumbers(ButtonText: "5", color: Colors.white38),
+              theNumbers(ButtonText: "6", color: Colors.white38),
+              theNumbers(ButtonText: "-", color: Colors.amber),
             ],
           ),
           Row(
             children: <Widget>[
-              TheNumbers(ButtonText: "1", color: Colors.white),
-              TheNumbers(ButtonText: "2", color: Colors.white),
-              TheNumbers(ButtonText: "3", color: Colors.white),
-              TheNumbers(ButtonText: "+", color: Colors.amber),
+              theNumbers(ButtonText: "1", color: Colors.white38),
+              theNumbers(ButtonText: "2", color: Colors.white38),
+              theNumbers(ButtonText: "3", color: Colors.white38),
+              theNumbers(ButtonText: "+", color: Colors.amber),
             ],
           ),
           Row(
             children: <Widget>[
-              TheNumbers(ButtonText: '0', color: Colors.white),
-//              TheNumbers(ButtonText: '.', color: Colors.white),
-              TheNumbers(ButtonText: "=", color: Colors.amber),
+              theNumbers(ButtonText: '0', color: Colors.amber),
+              theNumbers(ButtonText: '.', color: Colors.amber),
+              theNumbers(ButtonText: "=", color: Colors.amber),
             ],
           ),
         ],
